7329c4cd0329d443b95828cdc7ef4f0554868daf
DushanMalind <dushanmalinda010@gmail.com>
2025-02-08 16:29:40 +0530

    BookingSchema add timestamps done

--- models/Booking.js
+++ models/Booking.js
@@ -4,6 +4,6 @@ const BookingSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     service: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: true },
     slot: { type: Date, required: true }
-});
+},{timestamps: true});
 
 module.exports = mongoose.model('Booking', BookingSchema);
