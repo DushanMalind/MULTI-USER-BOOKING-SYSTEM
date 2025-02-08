f6eee4a7e44c0cf0dd903c13292cab82ead73c3a
DushanMalind <dushanmalinda010@gmail.com>
2025-02-08 21:23:12 +0530

    updateService deleteService  done

--- controller/serviceController.js
+++ controller/serviceController.js
@@ -23,3 +23,34 @@ exports.createService = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+exports.updateService = async (req, res) => {
+    try {
+        if (req.user.role !== 'admin') {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+        const service = await Service.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!service) {
+            return res.status(404).json({ message: "Service not found" });
+        }
+        res.json(service);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+exports.deleteService = async (req, res) => {
+    try {
+        if (req.user.role !== 'admin') {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+        const service = await Service.findByIdAndDelete(req.params.id);
+        if (!service) {
+            return res.status(404).json({ message: "Service not found" });
+        }
+        res.json({ message: "Service deleted" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
