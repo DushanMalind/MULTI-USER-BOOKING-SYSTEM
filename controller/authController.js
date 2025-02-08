1b17e51338de367e67fc5e034d29732cd89cd0f1
DushanMalind <dushanmalinda010@gmail.com>
2025-02-08 16:51:54 +0530

    login done

--- controller/authController.js
+++ controller/authController.js
@@ -14,3 +14,17 @@ exports.register = async (req, res) => {
     }
 };
 
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
