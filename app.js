ade9e9554d1c675170e99b8cea6b8f438de40baa
DushanMalind <dushanmalinda010@gmail.com>
2025-02-08 14:14:05 +0530

    app js db and cors add

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -39,3 +40,30 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+*/
+
+var express = require('express');
+const mongoose = require('mongoose');
+var bodyParser=require('body-parser');
+const cors=require('cors');
+
+/*All Route Files*/
+
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
+/* Route Api */
+
+
+const CONNECTION_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/multi-user-booking';
+const PORT = process.env.PORT|| 4000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
