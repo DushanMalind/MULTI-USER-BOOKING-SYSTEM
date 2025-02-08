c86d7baa40396965a0a9fabc8705620fef1f4402
DushanMalind <dushanmalinda010@gmail.com>
2025-02-08 17:02:14 +0530

    AuthMiddle Update

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+/*const jwt = require('jsonwebtoken');
 const UserModal = require('../models/User');
 
 const secret = 'test';
@@ -24,4 +24,30 @@ const AuthMiddleware = async (req, res, next) => {
     }
 };
 
-module.exports = AuthMiddleware;
+module.exports = AuthMiddleware;*/
+
+
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ message: "Access Denied" });
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: "Invalid Token" });
+    }
+};
+
+const authorizeAdmin = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: "Access Denied" });
+    }
+    next();
+};
+
+module.exports = { authenticate, authorizeAdmin };
+
