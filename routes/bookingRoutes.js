21f6711d53a197b8d6de12a9f2a2edaa2ed74535
DushanMalind <dushanmalinda010@gmail.com>
2025-02-08 17:40:07 +0530

    bookingRoutes bookings done

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { createBooking, getUserBookings, cancelBooking } = require('../controller/bookingController');
+const { authenticate } = require('../middleware/authMiddleware');
+const router = express.Router();
+
+router.post('/bookings', authenticate, createBooking);
+
+
+module.exports = router;
